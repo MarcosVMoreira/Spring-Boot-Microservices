ad6aa13d4fa2b6fe6a60ce028e8e65e4f90eb754
Marcos <contato.marcosvmoreira@gmail.com>
2020-04-22 11:23:03 -0300

    Add: setup inicial e modularizacao

---
+++ core/src/main/java/academy/devdojo/youtube/core/repository/CourseRepository.java
@@ -0,0 +1,8 @@
+package academy.devdojo.youtube.core.repository;
+
+
+import academy.devdojo.youtube.core.model.Course;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface CourseRepository extends PagingAndSortingRepository<Course, Long> {
+}
